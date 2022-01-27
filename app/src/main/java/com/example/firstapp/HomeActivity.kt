7b5437f84212590444ac34b1f88ac08d88b976eb
avichalcr7 <64785020+avichalcr7@users.noreply.github.com>
2022-01-27 12:32:46 +0530

    passing data

--- app/src/main/java/com/example/firstapp/HomeActivity.kt
+++ app/src/main/java/com/example/firstapp/HomeActivity.kt
@@ -3,16 +3,21 @@ package com.example.firstapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import android.widget.TextView
 import android.content.Intent
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        var name = intent.extras?.getString("ba")
+        var tvHome = findViewById<TextView>(R.id.tvHome)
+        tvHome.text = name
     }
     fun handleClick(view: android.view.View) {
         var intent: Intent
         intent = Intent(this,MainActivity::class.java)
+        intent.putExtra("ap","avichal pathak")
         startActivity(intent)
     }
 }
--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String name = getIntent().getExtras().getString("ap");
+        TextView mTextView = findViewById(R.id.tvMain);
+        mTextView.setText(name);
     }
     public void clickHandler(View view) {
         Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
         Intent mIntent;
         mIntent  = new Intent(MainActivity.this,HomeActivity.class);
+        mIntent.putExtra("ba","barry allen");
         startActivity(mIntent);
     }
 }
