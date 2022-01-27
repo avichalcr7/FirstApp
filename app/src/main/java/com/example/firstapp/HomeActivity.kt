5b88a041fcff667875ca54f101886fc1f50b7bbd
avichalcr7 <64785020+avichalcr7@users.noreply.github.com>
2022-01-27 14:52:15 +0530

    invoke activities

--- app/src/main/java/com/example/firstapp/HomeActivity.kt
+++ app/src/main/java/com/example/firstapp/HomeActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Toast
 import android.widget.TextView
 import android.content.Intent
+import android.net.Uri
+import android.view.View
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +17,23 @@ class HomeActivity : AppCompatActivity() {
         tvHome.text = name
     }
     fun handleClick(view: android.view.View) {
-        var intent: Intent
-        intent = Intent(this,MainActivity::class.java)
-        intent.putExtra("ap","avichal pathak")
-        startActivity(intent)
+        when(view.id){
+            R.id.btnSend -> { startDialer()}
+            R.id.btnMain -> { startMain()}
+        }
+        //startMain()
+        //startDialer()
+    }
+
+    private fun startDialer() {
+        var dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:8299266615"))
+        startActivity(dialIntent)
+    }
+
+    private fun startMain() {
+            var intent: Intent
+            intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("ap", "avichal pathak")
+            startActivity(intent)
     }
 }
